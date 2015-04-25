d6ea8a32e782dc844bca79b5ec2cb64735661b81
Quan Huynh <quanhuynh96@berkeley.edu>
2015-04-24 22:17:07 -0700

    updated smoothScroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,3 +5,21 @@ $(document).ready(
 );
 
 $("#mydiv").getNiceScroll().hide();
+
+$(function() {
+	smoothScroll(1000);
+})
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $( $(this).attr('href') );
+
+		if (target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration);
+		}
+
+	});
+}
